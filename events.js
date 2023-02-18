d7fbfd4cb2dae8793ae32b637ba6520de2cf32c1
Caleb Kandoro <kandoroc@hotmail.com>
2023-02-18 11:16:17 +0200

    dada

---
+++ events.js
@@ -0,0 +1,247 @@
+const events = [
+    {
+      'date': new Date(2022,8,18),
+      'photos': [],
+      'messages': []
+    },
+    {
+      'date': new Date(2022,8,25),
+      'photos': [
+        "https://lh3.googleusercontent.com/RqBMS6kDuihMJJJO5uy85vggfyBrNmN0MPD4U9VlR2sHkrs7dCuDhblUvHrq7N7MfQ74hED_qFM40PhxTAkepr3DvRfVNOBw_HSA5BNf59pBZYyV7IOeKE9x1vY11nK7rssKz7cAnw=w2400"
+      ],
+      'messages': []
+    },
+    
+    {
+      'date': new Date(2022,9,1),
+      'photos': [
+        "https://lh3.googleusercontent.com/NQFl3b8hfhIh6Wa82NPNT0hkj_HHGpNr_WQPG0cYaQ-4gc9oLNo3Ufr8QDM6UyQ1jLo6TrSODgei2i2qvk80Hw9v-XMnJoKM3JWo1vg7YA36jf65qbVhcDE3Ep7EYlrghNqvkrQ6bQ=w2400",
+        "https://lh3.googleusercontent.com/wEUFCgBBh0y9nwKYvOmsCO8Cn1RnpZUrT-hSwoo8-8pJ447-LQrd_TDFvgiinXb1lqHu7GrcNccnGUHEu6F9Tb6KC0nQZar5XqIOLur0og4v6Z7RJDLjWfb6hNqK8ZucFBc16GxKXA=w2400",
+        "https://lh3.googleusercontent.com/Nd1r1HVk2CNXTq7no47KIgWY98Xwl6cHvqfkTxXU7wFfcrtI82nAEatuFExPTBSw77ITd2zyXuqVu8BJ47KdCzBxkAtw5dCHK0H-032RhJnF4-Badvvhinz71z0Q0eHB9EElW1E3iw=w2400"
+      ],
+      'messages': []
+    },
+
+    {
+      'date': new Date(2022,9,4),
+      'photos': [],
+      'messages': [
+        {
+          "sent": true,
+          "time": "18:26",
+          "msg": "#007bff"
+        },
+        {
+          "sent": false,
+          "time": "18:26",
+          "msg": "The '02 in me reads this as #best friends forever with James Bond"
+        }
+      ]
+    },
+    
+    {
+      'date': new Date(2022,9,15),
+      'photos': [
+        "https://lh3.googleusercontent.com/ZecFEjUsc9CgBLMFpQjVTDdO3NVuPJgVTVIRbBDv3ZAw_EK5_Tx2hgD9AWGCVcsZs9Tq1e4bcmZGowEt0QD3njbrEqNvp7qH6i-n2jkalHjwQqhUOa82gOWuwX2UPHNrXqy5tuy0KQ=w2400"
+      ],
+      'messages': []
+    },
+    {
+        'date': new Date(2022,9,17),
+        'photos': [],
+        'messages': [
+            {
+                "sent": true,
+                "time": "17:46",
+                "msg": "Thing is you only like my hands, I love all of you."
+              },
+              {
+                "sent": false,
+                "time": "17:46",
+                "msg": "This isn't true though \n I mean your hands are (chef's kiss) but you have cute toes too. And a lovely smile. I recently learnt that the only pillow that's not likely to give me neck aches is your shoulder."
+              },
+              {
+                "sent": false,
+                "time": "17:51",
+                "msg": "Even before we were dating ( and I was nurturing a hideously large crush) whenever you'd smile or laugh while doing the watchtower I couldn't resist and I'd smile into my mask. Thank goodness for the masks!"
+              }
+        ]
+      },
+    
+    {
+      'date': new Date(2022,10,6),
+      'photos': [
+        "https://lh3.googleusercontent.com/xR3dRU_ppNt_U7mfH6LW_tq5G0Ib8xzetvM-aVGHZtt8fFCQRpihx-L9l8WYAoAJ8iJvirkt4KU_QnsQtXgXpKst4JVzoX04j50gRdWVHHqMLZFtNUJuoEW7ORdVxtsXjxjTZTHdSA=w2400",
+        "https://lh3.googleusercontent.com/WcgLamDqN30jhP6yQOnzsnxMlL1Mntc0vKQSKEbtHtoL_UZDHtwzW5oSj8V_zbrwLMuPJAjTKzB7qgIsvpyZ-zXKIbKbcZNrwYaFyhXXEElnqiog8dOjFhLrjk0Jgp4eRDO9UzMEFw=w2400"
+      ],
+      'messages': []
+    },
+
+    {
+        'date': new Date(2022,10,13),
+        'photos': [
+          "https://lh3.googleusercontent.com/eOlLLPO6HB5u3t1tyTlbYBOnJxonzkoNE6t1mJnzQ4HX-jDdUNXywo8T2NES1YMquZd4LgpKSOYZsUIzSUE0YchLB_MN2bMFJceKsicBuEJx-URODTzfcXuq4_eFuPyzLmUYdR9XNg=w2400",
+          "https://lh3.googleusercontent.com/-xSXBBYEhYvYCVffgIYdeB3P6wZEOth4gJ-NgtNtQPhhTlxWElfZb-ACLMj_w7IVCEGWxau4tvSuNl7PRngVN47AKyVSu4cKjChXtwNbymHfTmMP6-Lm5QD4LcOkG9IwnrJwoBTkFQ=w2400"
+        ],
+        'messages': []
+      },
+    
+    {
+      'date': new Date(2022,11,24),
+      'photos': [
+        "https://lh3.googleusercontent.com/FaRPsy5IWi5Qzi3-TlgnVjMranUkrNEvSJy7MtKjSHI1J7JcoVmkQUW2oFGxV-_-BfIru49DGwzpAn281qv_wrAQKb127Fob30ajxPrW-lJkYfhzYcjc2c1q96RwtjyZJT2isyaSVA=w2400",
+        "https://lh3.googleusercontent.com/YsAik8pr6F3drZqFgIjwGEvsjuwSREbp0vS2H5XQEwy9m8Adh63sL3EG4KNWCmv0xepiMF9C5mQC6YUVNI7YPZnWtwDOYscevarFVsTqaXh8-_Usk42zHJ-eF2SMtN859zgxi79lqQ=w2400"
+      ],
+      'messages': []
+    },
+    
+    {
+      'date': new Date(2022,11,25),
+      'photos': [
+        "https://lh3.googleusercontent.com/kzHu7P4DtrC16TmCsu9qVVvOKertmRlLssfkRE8WmiNkeVrVCX5aPyoe1Z3Zi1AC3OYQXgVn9AgU7mBDBiNg0Pns2z0u7DAQIG7pcNKoLASIZAgTo6pSJYGZU1kZz8k2xa0ZobyYGg=w2400",
+        "https://lh3.googleusercontent.com/r1zInf3RbXVmp6_lvd1VYtc6lLuxDWXPWeYeK2I_sEB15L35AfgJ3NT7KKXMNpaQ0gCAHH3F9DtLwxrjfyikIM5TPeS9tlw4BdFDNfpsRwKNIuOCIuLoqr2BNnHwMUfZ2s-nYFXibA=w2400"
+      ],
+      'messages': []
+    },
+
+    
+    {
+      'date': new Date(2022,11,27),
+      'photos': [
+        "https://lh3.googleusercontent.com/XX5tmOSTYIc0EcCqKuj0a2ZPigEwgpc1UgQ2F_QmNabCY-6DBPVnek26CiI3uZrJWqL1MVDdeA95R__J3s48ZQRSxOE4-W0YMmrPn6kLGhBZ7zPxP4lZ9uEDDTHXplJRtibSGTWaQg=w2400"
+      ],
+      'messages': []
+    },
+    
+    // {
+    //   'date': new Date(2023,0,2),
+    //   'photos': [],
+    //   'messages': []
+    // },
+    
+    // {
+    //   'date': new Date(2023,0,28),
+    //   'photos': [],
+    //   'messages': []
+    // },
+    
+    {
+      'date': new Date(2022,9,18),
+      'photos': [],
+      'messages': [
+        {
+            sent: false,
+            time: "05:25",
+            "msg": `Dear Caleb, 
+
+            I remember when I used to think good things would never happen for me and that no one could ever stand actually being with me. Now, I’ve always thought that couples were the cringiest for these “month-a-versarries” and I think I still do but like I said gooey couple stuff seems less sticky with you. Thank you for spending the last month encouraging, enduring and educating. It seems like the almost 4 months of agony I endured were worth it. Allow me to share a quote from the book I was telling you about last week. I think of it often whenever we’re on a date.
+            `
+        }
+      ]
+    },
+    {
+      'date': new Date(2022,9,21),
+      'photos': [],
+      'messages': [
+        {
+            sent: false,
+            time: "21:45",
+            msg: `It’s like forza but in this case you always win 1st place . The zone is not closing in any time soon and you’re most certainly not driving the Porsche!`
+        },
+        {
+            sent: true,
+            time: "21:46",
+            msg: `This is going to the top of my starred messages. I know you've said more meaningful and touching things but babe this pushes all my buttons`
+        },
+      ]
+    },
+    {
+      'date': new Date(2022,9,29),
+      'photos': [],
+      'messages': [
+        {
+            sent: false,
+            time: '21:26',
+            msg: `Because I prayed and I told Jehovah my specifications (because none of the brothers who were approaching me had them and I was tired😂) and then wham bam thank you ma’am here you come with all the specifications and you’re someone I know so it’s a much quicker process of seeing if you fit the bill. I agonized for weeks Caleb, weeks, I’d stare at you on the platform and like I told you I’d try pick out things to blow out of proportion to make me unlike you 😂but then we’d have these conversations or little things would happen like when I found out you liked Star Wars I surrendered and my prayers changed to be specifically about you. Help me find red flags in this brother or else prove to me that this is could last a lifetime and every time it was always the latter. So I took the plunge because I couldn’t keep praying for what was right in front of me. Then I started dating you. You know the sensation of falling and not being able to control it which is what happened with us and suddenly I was waking up and trying to think what it would be like if you were there too or looking up tiny homes and not freaking out about marriage and the future. I want those things with you. 
+
+            (I didn’t spell check so there may be appalling errors in here 😂)
+            `
+        }
+      ]
+    },
+    // {
+    //   'date': new Date(2022,10,2),
+    //   'photos': [],
+    //   'messages': []
+    // },
+    // {
+    //   'date': new Date(2022,10,18),
+    //   'photos': [],
+    //   'messages': []
+    // },
+
+    // {
+    //   'date': new Date(2022,11,21),
+    //   'photos': [],
+    //   'messages': []
+    // },
+
+    // {
+    //   'date': new Date(2022,11,28),
+    //   'photos': [],
+    //   'messages': []
+    // },
+    {
+      'date': new Date(2023,0,16),
+      'photos': [],
+      'messages': [
+        {
+            sent: false,
+            time: "07:43",
+            msg: "What terrifies me is what I would for you❤️"
+        },
+        {
+            sent: true,
+            time: "07:43",
+            msg: "Why?"
+        },
+        {
+            sent: false,
+            time: "07:43",
+            msg: "You know when people say you have to help me hide the body"
+        },
+        
+        {
+            sent: true,
+            time: "07:44",
+            msg: "😂😂(starred) perfect answer with immaculate delivery 🤌🏽🤌🏽"
+        }
+      ]
+    },
+    {
+      'date': new Date(2023,0,18),
+      'photos': [],
+      'messages': [
+        {
+            sent: false,
+            time: '13:14',
+            msg: "When I’m your wife I get to be around during times like this. And I get to hold you and you don’t have to complain about the twin peaks because by then you’ll have scaled them quite a number of times"
+        }
+      ]
+    },
+    
+    {
+        'date': new Date(2023,1,1),
+        'photos': [],
+        'messages': [
+            {
+                sent: false,
+                time: '08:13',
+                msg: `Before I was your girlfriend: you’re thoughtful and pensive. It’s adorable how you like explaining how things work. You give thoughtful comments, you’re an eloquent speaker and you’re so mannered and polite. You have a beautiful smile and you aren’t one of those people that likes things and is all over the place. 
+
+                After: your side profile when you’re giving driving commentary, the way you hold my hand. Your spontaneous song outbreaks. How considerate yet morally grey you are. How you never minimize my feelings. You’re funny and you’re never rude to waitstaff I could really go on…
+                `
+            }
+        ]
+      },
+  ]
--- timeline.js
+++ timeline.js
@@ -1,18 +1,4 @@
-const events = [
-    {
-      'date': new Date(2022,8,18),
-      'photos': [
-        "https://lh3.googleusercontent.com/BfVKgk6vcmCG6iS_H2kumjgXXUwAJDyWNMckL_x9uwiNKI8iZFxXwcTAx2mp9zjaL9W_7j3wpSGRGVvhR3-UVaffvGE92KSDaPBhLZHPpiBfdCHznQTlaNlOHLC-ShebyXqaH6sBkA=w2400"
-      ]
-    },
-    {
-      'date': new Date(2022,8,25),
-      'photos': [
-        "https://lh3.googleusercontent.com/BfVKgk6vcmCG6iS_H2kumjgXXUwAJDyWNMckL_x9uwiNKI8iZFxXwcTAx2mp9zjaL9W_7j3wpSGRGVvhR3-UVaffvGE92KSDaPBhLZHPpiBfdCHznQTlaNlOHLC-ShebyXqaH6sBkA=w2400"
-      ]
-    },
-    
-  ]
+
   
   const start = new Date(2022,8,18)
   const dayInMs = 24 * 3600 * 1000
@@ -27,15 +13,15 @@ const events = [
   timeline.style.width = `${(span + 3) * 200}px`
   dummy.style.width = `${(span + 3) * 200}px`
   
-  events.forEach(evt => {
+  events.forEach((evt, i) => {
     const delta = (evt.date - start) / dayInMs
     const container = document.querySelector('.ticker-layer')
     container.innerHTML += `<div class="ticker" style="left: ${delta * 200}px;">
             <div class="callout">
                 <div class="ticker-label">${evt.date.toDateString()}</div>
                 <div class="button-container">
-                    <div class="button"><i class="fa-regular fa-comments fa-3x"></i></div>
-                    <div class="button"><i class="fa-regular fa-image fa-3x"></i></div>
+                  ${evt.photos.length > 0 ? `<div class="button" onclick="renderImages(${i})"><i class="fa-regular fa-image fa-3x"></i></div> `: ""}      
+                  ${evt.messages.length > 0 ? `<div class="button" onclick="renderMessages(${i})"><i class="fa-regular fa-comments fa-3x"></i></div> `: ""}
                 </div>
             </div>
             <div class="ticker-stem"></div>
@@ -44,3 +30,142 @@ const events = [
         </div>`
   })
   
+
+  const track = document.querySelector('.timeline-container')
+  const skybox = document.querySelector('.skybox')
+
+
+const calculateDayColour = () => {
+  const dayLight = [135,206,235]
+  const night = [25,25,112]
+  const delta = dayLight.map((v,i) => v - night[i])
+  const modulo = 1400
+  
+  const position = track.scrollLeft
+
+  let ratio = (position % modulo) / modulo
+  ratio = ratio > 0.5 ? 2- (ratio * 2) : ratio * 2
+  const color = dayLight.map((v, i) => v - parseInt(delta[i] * ratio))
+  skybox.style.background = `rgb(${color[0]}, ${color[1]}, ${color[2]})` 
+}
+
+
+const calculateSunPosition = () => {
+  const angle = 180
+  const modulo = 1400
+  const origin = [0, 50]
+  const midday = [50, 5]
+  const sunset = [100, 50]
+  
+  const position = track.scrollLeft
+  let ratio = (position % modulo) / modulo
+  
+
+  const sun = document.querySelector('.sun')
+  
+  if(ratio > 0.25 && ratio < 0.75) {
+    sun.style.display = 'none'
+    return
+  } else {
+    sun.style.display = 'block'
+  }
+
+  let sunX = 0
+  let sunY = 50
+
+  
+
+  
+  if(ratio <= 0.25) {
+    ratio = ratio / 0.25
+  } else {
+    ratio = (1 - ratio) / 0.25
+  }
+
+  sunY = ratio * 100
+  sunX = ratio * 100
+
+  sun.style.top = `${sunY}vh`
+  sun.style.left = `${sunY}vw`
+}
+
+const calculateMoonPosition = () => {
+  const dayLight = [135,206,235]
+  const night = [25,25,112]
+  const delta = dayLight.map((v,i) => v - night[i])
+  const modulo = 1400
+  
+  const position = track.scrollLeft
+  console.log(position)
+  let ratio = (position % modulo) / modulo
+  ratio = ratio > 0.5 ? 2- (ratio * 2) : ratio * 2
+  const color = dayLight.map((v, i) => v - parseInt(delta[i] * ratio))
+  track.style.background = `rgb(${color[0]}, ${color[1]}, ${color[2]})`
+}
+
+
+track.addEventListener('scroll', evt => {
+  calculateDayColour()
+  // calculateSunPosition()
+})
+
+const starContainer = document.querySelector('.stars')
+
+
+Array.apply(null, Array(100)).forEach(i => {
+  const width = parseInt(Math.random() * starContainer.clientWidth)
+  const height = parseInt(Math.random() * starContainer.clientHeight)
+  starContainer.innerHTML += `<div style="left: ${width}px; top: ${height}px;"></div>`
+})
+
+
+const renderMessages = idx => {
+  const phoneContainer = document.querySelector('.phone-container')
+  const msgList = document.querySelector('.messages')
+  msgList.innerHTML = ""
+  const messages = events[idx].messages
+  messages.forEach(msg => {
+    msgList.innerHTML += msg.sent 
+      ? `<div class="message-sent">${msg.msg}<div class="time">${msg.time}</div></div>`
+      : `<div class="message-received">${msg.msg}<div class="time">${msg.time}</div></div>`
+  })
+  phoneContainer.style.display=  "flex"
+
+}
+
+const hideMessages = () => {
+  const phoneContainer = document.querySelector('.phone-container')
+  phoneContainer.style.display=  "none"
+
+}
+
+const renderImages = idx => {
+  const modal = document.querySelector('.modal')
+  const imgList = document.querySelector('.img-selector')
+  const current = document.querySelector('#current-img')
+  const links = events[idx].photos
+  current.src = links[0]
+  
+  modal.style.display=  "flex"
+
+  imgList.innerHTML = ""
+  links.forEach(l => {
+    imgList.innerHTML += `
+      <div class="thumbnail" onclick="setCurrentImg('${l}')">
+        <img src="${l}" />
+      </div>
+    `
+  })
+}
+
+const setCurrentImg = (url) => {
+  const current = document.querySelector('#current-img')
+  current.src = url
+
+}
+
+const hideModal = () => {
+  const phoneContainer = document.querySelector('.modal')
+  phoneContainer.style.display=  "none"
+
+}
